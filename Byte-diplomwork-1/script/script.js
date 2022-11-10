c4b5b4aecd2c6fc3927847c9f6d4371d70713fb5
Andrew <114303256+AnMoor@users.noreply.github.com>
2022-11-11 00:38:52 +0200

    Add files via upload

---
+++ Byte-diplomwork-1/script/script.js
@@ -0,0 +1,10 @@
+$('.hiring-about >div').on('click', function(){
+    $(this).children('.hiring-icon').toggleClass('hiring-rotate-icon');
+    $(this).parent('.hiring-about').toggleClass('change-color')
+    $(this).next('.hiring-text').slideToggle(300);
+})
+
+$('.futures-subblock >div').on('click', function(){
+    $(this).parent('.futures-subblock').toggleClass('futures-block-open')
+    $(this).next('.futures-block-text').slideToggle(300);
+})
